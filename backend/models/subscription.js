3367977dc3614ca8b69e3bb68dcb5430f08bf035
hoggbox <94619876+hoggbox@users.noreply.github.com>
2025-04-04 15:54:25 -0400

    Create subscription.js

---
+++ backend/models/subscription.js
@@ -0,0 +1,13 @@
+// models/subscription.js
+const mongoose = require('mongoose');
+
+const SubscriptionSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  endpoint: { type: String, required: true },
+  keys: {
+    p256dh: { type: String, required: true },
+    auth: { type: String, required: true }
+  }
+});
+
+module.exports = mongoose.model('Subscription', SubscriptionSchema);
