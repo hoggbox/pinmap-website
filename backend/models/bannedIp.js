77161e35cffc7e6baad6b2a3d6674da40ff78420
hoggbox <94619876+hoggbox@users.noreply.github.com>
2025-04-18 09:48:11 -0400

    Delete backend directory

--- backend/models/bannedIp.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const bannedIpSchema = new mongoose.Schema({
-  ipAddress: { type: String, required: true },
-  bannedAt: { type: Date, default: Date.now },
-  unbannedAt: { type: Date },
-  reason: String,
-  adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-});
-module.exports = mongoose.model('BannedIP', bannedIpSchema);
